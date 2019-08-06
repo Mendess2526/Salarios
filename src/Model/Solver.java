baabe204a30c022db8adfeab03b2df665c70c9f4
Mendess2526 <pedro.mendes.26@gmail.com>
2019-08-06 15:04:24 +0100

    Fix math

--- src/Model/Solver.java
+++ src/Model/Solver.java
@@ -2,32 +2,32 @@ package Model;
 
 public class Solver {
     private static int totalIliquido(int salarioBruto, int subAlimNisento, int diuturnidadesNisentos,
-                                    int subAlimentoIsentoValor, int abonoFalhasIsento, int outrosIsentos){
+                                     int subAlimentoIsentoValor, int abonoFalhasIsento, int outrosIsentos) {
         return salarioBruto + subAlimNisento + diuturnidadesNisentos + subAlimentoIsentoValor + abonoFalhasIsento +
                 outrosIsentos;
     }
 
-    private static double segSocial(int salariobruto, int subAlimNisento, int diuturnidadesNisentos){
-        return (salariobruto+subAlimNisento+diuturnidadesNisentos)*0.11;
+    private static int segSocial(int salariobruto, int subAlimNisento, int diuturnidadesNisentos) {
+        return (int) Math.round((salariobruto + subAlimNisento + diuturnidadesNisentos) * 0.11);
     }
 
-    private static double retencaoIRS(int salarioBruto, int subAlimNisento, int diuturnidadesNisentos){
-        return (salarioBruto+subAlimNisento+diuturnidadesNisentos)*0.64;
+    private static int retencaoIRS(int salarioBruto, int subAlimNisento, int diuturnidadesNisentos) {
+        return (int) Math.round((salarioBruto + subAlimNisento + diuturnidadesNisentos) * 0.64);
     }
 
-    private static double totalLiquido(int totalIliquido, double segSocial, double retencaoIRS){
-        return totalIliquido-segSocial-retencaoIRS;
+    private static int totalLiquido(int totalIliquido, int segSocial, int retencaoIRS) {
+        return totalIliquido - segSocial - retencaoIRS;
     }
 
-    private static double segSocialEEE(int salariobruto, int subAlimNisento, int diuturnidadesNisentos){
-        return (salariobruto+subAlimNisento+diuturnidadesNisentos)*0.2375;
+    private static int segSocialEEE(int salariobruto, int subAlimNisento, int diuturnidadesNisentos) {
+        return (int) Math.round((salariobruto + subAlimNisento + diuturnidadesNisentos) * 0.2375);
     }
 
-    private static double FCT(int salarioBruto, int diuturnidadesNisentos){
-        return (salarioBruto+diuturnidadesNisentos)*0.1;
+    private static int FCT(int salarioBruto, int diuturnidadesNisentos) {
+        return (int) Math.round((salarioBruto + diuturnidadesNisentos) * 0.1);
     }
 
-    private  int salarioBruto;
+    private int salarioBruto;
     private int subAlimNisento;
     private int diuturnidadesNisentos;
     private int subAlimIsento;
@@ -41,22 +41,34 @@ public class Solver {
     private int FCT;
 
 
-    public OutputData solve(InputData in){
+    public OutputData solve(InputData in) {
         int salarioBruto = in.getSalarioBruto();
         int subAlimNisento = in.getSubAlimNisento();
         int diuturnidadesNisentos = in.getDiuturnidadesNisentos();
         int subAlimIsento = in.getSubAlimIsentoValor();
         int abonoFalhasIsento = in.getAbonoFalhasIsento();
         int outrosIsentos = in.getOutrosIsentos();
-        int totalIliquido = totalIliquido(salarioBruto, subAlimNisento, diuturnidadesNisentos, subAlimIsento,
-                abonoFalhasIsento, outrosIsentos);
-        double segSocial = segSocial(salarioBruto, subAlimNisento, diuturnidadesNisentos);
-        double retencaoIRS = retencaoIRS(salarioBruto, subAlimNisento, diuturnidadesNisentos);
-        double totalLiquido = totalLiquido(totalIliquido, segSocial, retencaoIRS);
-        double segSocialEEE = segSocialEEE(salarioBruto, subAlimNisento, diuturnidadesNisentos);
-        double FCT = FCT(salarioBruto, diuturnidadesNisentos);
-        OutputData out = new OutputData(salarioBruto, subAlimNisento, diuturnidadesNisentos, subAlimIsento,
-                abonoFalhasIsento, outrosIsentos, totalIliquido, segSocial, retencaoIRS, totalLiquido, segSocialEEE, FCT);
+
+        int totalIliquido = totalIliquido(salarioBruto, subAlimNisento, diuturnidadesNisentos, subAlimIsento, abonoFalhasIsento, outrosIsentos);
+        int segSocial = segSocial(salarioBruto, subAlimNisento, diuturnidadesNisentos);
+        int retencaoIRS = retencaoIRS(salarioBruto, subAlimNisento, diuturnidadesNisentos);
+        int totalLiquido = totalLiquido(totalIliquido, segSocial, retencaoIRS);
+        int segSocialEEE = segSocialEEE(salarioBruto, subAlimNisento, diuturnidadesNisentos);
+        int FCT = FCT(salarioBruto, diuturnidadesNisentos);
+
+        OutputData out = new OutputData(
+                salarioBruto,
+                subAlimNisento,
+                diuturnidadesNisentos,
+                subAlimIsento,
+                abonoFalhasIsento,
+                outrosIsentos,
+                totalIliquido,
+                segSocial,
+                retencaoIRS,
+                totalLiquido,
+                segSocialEEE,
+                FCT);
         return out;
     }
 }
