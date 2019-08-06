f0a3ada979d15668b44146a7ff7d398d82052514
Mendess2526 <pedro.mendes.26@gmail.com>
2019-08-06 15:23:04 +0100

    Change constantes to double

--- src/Model/Constants.java
+++ src/Model/Constants.java
@@ -3,24 +3,24 @@ package Model;
 import java.security.InvalidParameterException;
 
 public class Constants {
-    public static final int SocialSecurityTaxWorker = 1100;
-    public static final int SocialSecurityTaxEmployer = 2375;
-    public static final int FCT = 100;
+    public static final double SocialSecurityTaxWorker = 0.1100;
+    public static final double SocialSecurityTaxEmployer = 23.75;
+    public static final double FCT = 0.100;
 
-    public static int retentionTax(MaritalStatus m) {
+    public static double retentionTax(MaritalStatus m) {
         switch (m) {
-            case Single: return 350;
-            case Married1: return 120;
-            case Married2: return 640;
+            case Single: return 3.50;
+            case Married1: return 1.20;
+            case Married2: return 6.40;
             default: throw new InvalidParameterException();
         }
     }
 
-    public static int retentionTax2(MaritalStatus m) {
+    public static double retentionTax2(MaritalStatus m) {
         switch (m) {
-            case Single: return 2980;
-            case Married1: return 2410;
-            case Married2: return 3140;
+            case Single: return 29.80;
+            case Married1: return 24.10;
+            case Married2: return 31.40;
             default: throw new InvalidParameterException();
         }
     }
